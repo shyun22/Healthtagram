3187617e4e9cbed8caf0fb2e87aa4a990d8b0d1a
seunghyun <h_sl@hanmail.net>
2023-02-19 11:16:46 +0900

    axios

---
+++ src/App.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="header">
+    <ul class="header-button-left">
+      <li>Cancel</li>
+    </ul>
+    <ul class="header-button-right">
+      <li>Next</li>
+    </ul>
+    <img src="./assets/logo.png" class="logo" />
+  </div>
+
+  <ContainerView :data="data" />
+  <button @click="more(), clickCountUp()">더보기</button>
+
+  <div class="footer">
+    <ul class="footer-button-plus">
+      <input type="file" id="file" class="inputfile" />
+      <label for="file" class="input-plus">+</label>
+    </ul>
+  </div>
+
+  <div>내용0</div>
+  <div>내용1</div>
+  <div>내용2</div>
+  <button>버튼0</button>
+  <button>버튼1</button>
+<button>버튼2</button>
+</template>
+
+<script>
+import ContainerView from "./components/ContainerView";
+import data from "./assets/data.js"
+import axios from 'axios'
+axios.get()
+
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      data: data,
+      clickCount: 0,
+      tabContent: 0,
+
+    }
+  },
+  components: {
+    ContainerView,
+
+  },
+  methods: {
+    more() {
+      axios.get(`https://codingapple1.github.io/vue/more${this.clickCount}.json`)
+        .then((결과) => {
+          console.log(결과);
+          this.data.push(결과.data);
+        })
+    },
+    clickCountUp() {
+      this.clickCount += 1;
+    }
+
+  },
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+ul {
+  padding: 5px;
+  list-style-type: none;
+}
+
+.logo {
+  width: 22px;
+  margin: auto;
+  display: block;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 13px;
+}
+
+.header {
+  width: 100%;
+  height: 40px;
+  background-color: white;
+  padding-bottom: 8px;
+  position: sticky;
+  top: 0;
+}
+
+.header-button-left {
+  color: skyblue;
+  float: left;
+  width: 50px;
+  padding-left: 20px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+
+.header-button-right {
+  color: skyblue;
+  float: right;
+  width: 50px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+
+.footer {
+  width: 100%;
+  position: sticky;
+  bottom: 0;
+  padding-bottom: 10px;
+  background-color: white;
+}
+
+.footer-button-plus {
+  width: 80px;
+  margin: auto;
+  text-align: center;
+  cursor: pointer;
+  font-size: 24px;
+  padding-top: 12px;
+}
+
+.sample-box {
+  width: 100%;
+  height: 600px;
+  background-color: bisque;
+}
+
+.inputfile {
+  display: none;
+}
+
+.input-plus {
+  cursor: pointer;
+}
+
+#app {
+  box-sizing: border-box;
+  font-family: "consolas";
+  margin-top: 60px;
+  width: 100%;
+  max-width: 460px;
+  margin: auto;
+  position: relative;
+  border-right: 1px solid #eee;
+  border-left: 1px solid #eee;
+}
+</style>
---
+++ src/components/ContainerView.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <PostView :data="data[i]" v-for="(a, i) in data" :key="i" />
+</div>
+</template>
+
+<script>
+import PostView from "./PostView"
+
+
+export default {
+    name: "ContainerView",
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        PostView,
+    },
+    props: {
+        data: Array,
+
+    },
+
+}
+</script>
+
+<style></style>
